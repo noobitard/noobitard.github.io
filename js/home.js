6ce2823ef26289c339307110bb4d41fdd6bd6ea8
pc <hidevn@yahoo.com>
2021-12-21 01:10:37 +0700

    Change homepage

---
+++ js/home.js
@@ -0,0 +1,33 @@
+var randomCovers = [
+    "https://www.youtube.com/embed/2DruyPVMMx8",
+    "https://www.youtube.com/embed/b2r5fYvHnbs", 
+    "https://www.youtube.com/embed/_fjgZHhZ-Y4",
+    "https://www.youtube.com/embed/8XXRh2a2Boc",
+    "https://www.youtube.com/embed/mobWZBnivJw",
+    "https://www.youtube.com/embed/2rMEji9wKa8",
+    "https://www.youtube.com/embed/qNIyobivZzE",
+    "https://www.youtube.com/embed/tPt4iJhaYI8",
+    "https://www.youtube.com/embed/dTvTG977dcg"
+]
+
+$(document).ready(()=>{
+    $('#music-note').css('-webkit-animation','animated-icon 1s 100ms  ease-in-out both infinite');
+    $('#academic-book').css('-webkit-animation','animated-icon 1s 100ms  ease-in-out both infinite');
+
+    $("#music-note").click(()=>{
+        $('body').css('overflow', 'hidden')
+        $("#academic-home-container").fadeToggle(500);
+        $("#music-home-container").fadeToggle(500);
+        $('body').animate({backgroundColor: '#424549', color: '#FFFFFF'}, 500);
+        var source = randomCovers[Math.floor(Math.random()*randomCovers.length)];
+        $("#youtube-player").prop("src", source);
+    })
+
+    $("#academic-book").click(()=>{
+        $('body').css('overflow', 'hidden')
+        $("#academic-home-container").fadeToggle(500);
+        $("#music-home-container").fadeToggle(500);
+        $('body').animate({backgroundColor: '#f2f2f2', color: '#000000'}, 500);
+        $("#youtube-player").prop("src", "");
+    })
+})
